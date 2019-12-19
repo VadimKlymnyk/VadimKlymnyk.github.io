30919225e6a1fe400413df1645c1791e742f3e1a
VadimKlymnyk <klumnukv@gmail.com>
2019-12-19 23:00:08 +0200

    JS

---
+++ test_JS/script.js
@@ -0,0 +1,79 @@
+
+function funCheck() {
+    let checkBox = document.getElementById("check_1");
+    let par = document.getElementsByClassName('container_item');
+    if ( checkBox.checked ) {
+        par[0].classList.add("gray");
+        console.log(1);
+    } else {
+        par[0].classList.remove("gray");
+        console.log(2);
+
+    }
+    
+
+}
+function funCheck2() {
+    let checkBox2 = document.getElementById("check_2");
+    let par = document.getElementsByClassName('container_item');
+    if ( checkBox2.checked ) {
+        par[1].classList.add("gray");
+        console.log(3);
+    } else {
+        par[1].classList.remove("gray");
+        console.log(4);
+
+    }
+}
+function funCheck3() {
+    let checkBox3 = document.getElementById("check_3");
+    let par = document.getElementsByClassName('container_item');
+    if ( checkBox3.checked ) {
+        par[2].classList.add("gray");
+        console.log(1);
+    } else {
+        par[2].classList.remove("gray");
+        console.log(2);
+
+    }
+    
+
+}
+function funCheck4() {
+    let checkBox4 = document.getElementById("check_4");
+    let par = document.getElementsByClassName('container_item');
+    if ( checkBox4.checked ) {
+        par[3].classList.add("gray");
+        console.log(3);
+    } else {
+        par[3].classList.remove("gray");
+        console.log(4);
+
+    }
+}
+function funCheck5() {
+    let checkBox5 = document.getElementById("check_5");
+    let par = document.getElementsByClassName('container_item');
+    if ( checkBox5.checked ) {
+        par[4].classList.add("gray");
+        console.log(1);
+    } else {
+        par[4].classList.remove("gray");
+        console.log(2);
+
+    }
+    
+
+}
+function funCheck6() {
+    let checkBox6 = document.getElementById("check_6");
+    let par = document.getElementsByClassName('container_item');
+    if ( checkBox6.checked ) {
+        par[5].classList.add("gray");
+        console.log(3);
+    } else {
+        par[5].classList.remove("gray");
+        console.log(4);
+
+    }
+}
