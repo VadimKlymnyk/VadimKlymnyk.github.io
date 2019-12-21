4ef85972bc2afad7afa6dd3edc6d0df832c3b763
VadimKlymnyk <klumnukv@gmail.com>
2019-12-21 20:08:42 +0200

    localStorage

--- test_JS/script.js
+++ test_JS/script.js
@@ -1,7 +1,18 @@
 
+for(let i =0; i<6; i++){
+    if(localStorage.getItem(`check_${i}`) == 1){
+        document.getElementById(`check_${i}_container`).classList.add("gray");
+        document.getElementById(`check_${i}`).checked = true;
+    }
+}
+
+
+
 function funCheck() {
-   
+  
+    localStorage[event.target.id] = document.getElementById(event.target.id).checked ? 1 : 0
+    console.log(localStorage.getItem(event.target.id));
     document.getElementById(`${event.target.id}_container`).classList.toggle("gray");
-    console.log(document.getElementById(`${event.target.id}_container`).classList)
+    
     
 }
